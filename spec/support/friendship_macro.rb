7805dccffb8aa3b02b9d67928c8048a1aa55e189
Sung Won Cho <mikeswcho@gmail.com>
2014-12-01 21:28:33 +1100

    Update Macros to use HasFriendship::Friendship rather than Friendship

--- spec/support/friendship_macro.rb
+++ spec/support/friendship_macro.rb
@@ -1,15 +1,15 @@
 module FriendshipMacros
   def create_request(friendable, friend)
-    Friendship.create(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id, status: 'pending')
-    Friendship.create(friendable_id: friend.id, friendable_type: friend.class.base_class.name, friend_id: friendable.id, status: 'requested')
+    HasFriendship::Friendship.create(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id, status: 'pending')
+    HasFriendship::Friendship.create(friendable_id: friend.id, friendable_type: friend.class.base_class.name, friend_id: friendable.id, status: 'requested')
   end
 
   def create_friendship(friendable, friend)
-    Friendship.create(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id, status: 'accepted')
-    Friendship.create(friendable_id: friend.id, friendable_type: friend.class.base_class.name, friend_id: friendable.id, status: 'accepted')
+    HasFriendship::Friendship.create(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id, status: 'accepted')
+    HasFriendship::Friendship.create(friendable_id: friend.id, friendable_type: friend.class.base_class.name, friend_id: friendable.id, status: 'accepted')
   end
 
   def find_friendship_record(friendable, friend)
-    Friendship.find_by(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id)
+    HasFriendship::Friendship.find_by(friendable_id: friendable.id, friendable_type: friendable.class.base_class.name, friend_id: friend.id)
   end
 end
