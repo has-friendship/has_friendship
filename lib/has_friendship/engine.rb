c65d4a537321e252103bf76265c98bdd49795b00
Kevin Brown <chevinbrown@gmail.com>
2018-10-18 09:28:42 -0400

    Do not include migrations

--- lib/has_friendship/engine.rb
+++ lib/has_friendship/engine.rb
@@ -1,13 +1,5 @@
 module HasFriendship
   class Engine < ::Rails::Engine
     isolate_namespace HasFriendship
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
   end
 end
