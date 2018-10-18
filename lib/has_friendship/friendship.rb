621de69439c352265e1267e10a93657ec6028acc
Kevin Brown <chevinbrown@gmail.com>
2018-10-17 22:25:46 -0400

    Add after_destroy hook

--- lib/has_friendship/friendship.rb
+++ lib/has_friendship/friendship.rb
@@ -5,6 +5,10 @@ module HasFriendship
       friend.on_friendship_created(self)
     end
 
+    after_destroy do
+      friend.on_friendship_destroyed(self)
+    end
+
     enum status: { pending: 0, requested: 1, accepted: 2, blocked: 3 } do
       event :accept do
         transition [:pending, :requested] => :accepted
