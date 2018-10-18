c1eddea6930c5ad8ba487d88c8aa0a91a9a7fd26
Kevin Brown <chevinbrown@gmail.com>
2018-10-18 09:36:01 -0400

    Remove generators
    By using a rails engine, we no longer need to maintain generators to
    produce migrations. :tada:

--- lib/generators/has_friendship/templates/add_blocker_id_to_friendships.rb
+++
@@ -1,9 +0,0 @@
-class AddBlockerIdToFriendships < ActiveRecord::Migration[4.2]
-  def self.up
-    add_column :friendships, :blocker_id, :integer, default: nil
-  end
-
-  def self.down
-    remove_column :friendships, :blocker_id
-  end
-end
