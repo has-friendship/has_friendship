b49ac222a167925273b6c2144ebb0ce3050c8827
Paul Walrath <paul@walrath.ca>
2025-12-20 10:53:30 -0500

    Fix testdb initialization

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -10,27 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2014_12_02_075742) do
-  create_table "friendships", force: :cascade do |t|
-    t.integer "blocker_id"
-    t.datetime "created_at"
-    t.integer "friend_id"
-    t.integer "friendable_id"
-    t.string "friendable_type"
-    t.integer "status"
-    t.datetime "updated_at"
-    t.index ["status"], name: "index_friendships_on_status"
-  end
+ActiveRecord::Schema.define(version: 4) do
 
-  create_table "unfriendables", force: :cascade do |t|
+  create_table "friendships", force: :cascade do |t|
+    t.string "friendable_type"
+    t.integer "friendable_id"
+    t.integer "friend_id"
     t.datetime "created_at"
-    t.string "name"
     t.datetime "updated_at"
+    t.integer "blocker_id"
+    t.integer "status"
+    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at"
     t.string "name"
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 end
