fcb72695393100dff590389a10f0e8c78c3fb546
Paul Walrath <paul@walrath.ca>
2025-12-30 21:31:02 -0500

    Adding second polymorphic model for tests

--- spec/has_friendship/feature_spec.rb
+++ spec/has_friendship/feature_spec.rb
@@ -3,29 +3,61 @@ require 'rails_helper'
 # Might not be needed
 describe "HasFriendship API" do
 
-  let(:user){ User.create(name: "Jessie") }
-  let(:friend){ User.create(name: "Heisenberg") }
+  let(:user){ User.create(name: "Jessie", id: 100) }
+  let(:friend){ User.create(name: "Heisenberg", id: 101) }
 
 	describe "Friend request" do
+
     context "when sent" do
+      before { user.friend_request(friend) }
+
       it "should create pending_friends association" do
-        user.friend_request(friend)
-        expect(user.pending_friends).to include friend
+        expect(user.pending_friends).to include(friend)
       end
 
       it "should create requested_friends association" do
-        user.friend_request(friend)
-        expect(friend.requested_friends).to include user
+        expect(friend.requested_friends).to include(user)
       end
     end
 
-    context "when accepted" do
+    context "when sent and accepted" do
+      before { user.friend_request(friend) }
+      before { friend.accept_request(user) }
+
       it "should create friends association" do
-        user.friend_request(friend)
-        friend.accept_request(user)
-        expect(user.friends).to include friend
-        expect(friend.friends).to include user
+        expect(user.friends).to include(friend)
+        expect(friend.friends).to include(user)
+      end
+    end
+
+    describe "When Friendships is a polymorphic relation" do
+      let(:tom){ Pet.create(name: "Tom", id: 100) }
+      let(:jerry){ Pet.create(name: "Jerry", id: 101) }
+
+      before do
+        tom.friend_request(jerry)
+        jerry.accept_request(tom)
+      end
+
+      context "when different friendable_type is added to the table" do
+        it "creates association using that friendable_type" do
+          expect(tom.friends).to include(jerry)
+          expect(jerry.friends).to include(tom)
+          expect(jerry.friendships.last.friendable_type).to eq(tom.class.to_s)
+        end
+      end
+
+      context "when other friendships records use the same IDs for a different table" do
+        before do
+          user.friend_request(friend)
+          friend.accept_request(user)
+        end
+
+        it "supports all the different friendable_type records" do
+          expect(HasFriendship::Friendship.count).to eq(4)
+        end
       end
     end
   end
+
 end
--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -11,16 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 4) do
-
   create_table "friendships", force: :cascade do |t|
     t.string "friendable_type"
     t.integer "friendable_id"
     t.integer "friend_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer "blocker_id"
     t.integer "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
